3b913bc8c93a9c123d8c0fdec7b85498299c3b3c
Walter Verwer <walterverwer@gmail.com>
2021-01-24 12:02:41 +0100

    get data

--- assignment1/ass1_py.py
+++ assignment1/ass1_py.py
@@ -8,6 +8,7 @@ Created on Sun Jan 24 11:14:05 2021
 import yfinance as yf
 import datetime
 import pandas as pd
+import os.path
 
 # constants:
 stocks = ['^DJI', 'GS', 'MCD', 'DOW', 'CAT', 'MRK', 'CVX', 'VZ', 
@@ -17,15 +18,21 @@ stocks = ['^DJI', 'GS', 'MCD', 'DOW', 'CAT', 'MRK', 'CVX', 'VZ',
 start_data = '2019-03-20' # important: stock 'DOW' is observed from this date onwards
 end_data = '2020-12-31'
 
-# get all stocks:
-counter = 0
-for stock in stocks:
-    if counter == 0:
-        df = pd.DataFrame(yf.download(stock, start=start_data, end=end_data)['Adj Close'])
-        df[stock] = df['Adj Close']
-        df.drop('Adj Close', inplace=True, axis = 1)
-    else:
-        df[stock] = yf.download(stock, start=start_data, end=end_data)['Adj Close']
-    counter += 1
+
+if os.path.isfile('dow_jones_stocks.csv'): # check if data exists:
+    df = pd.read_csv('dow_jones_stocks.csv')
+    
+else: # get all stocks using yahoo finance api:
+    counter = 0 # used to handle the first stock in the list
+    for stock in stocks:
+        if counter == 0:
+            df = pd.DataFrame(yf.download(stock, start=start_data, end=end_data)['Adj Close'])
+            df[stock] = df['Adj Close']
+            df.drop('Adj Close', inplace=True, axis = 1)
+        else:
+            df[stock] = yf.download(stock, start=start_data, end=end_data)['Adj Close']
+        counter += 1 
+    # save to csv:
+    df.to_csv('dow_jones_stocks.csv')
     
 
